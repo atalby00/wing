a4a62d5f86a59dbc38b0653b0ef890b00926253e
Achraf <achraf.talby@devoteam.com>
2023-09-18 08:08:08 +0200

    front: add use callback to useFetchParcels hook functions

--- front/src/hooks/useFetchParcels.ts
+++ front/src/hooks/useFetchParcels.ts
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { fetcDropParcelsOperation } from "../services/parcel.service";
 import { Parcel } from "../core/interfaces/parcel.interface";
 
@@ -14,24 +14,27 @@ export const useFetchParcels = (): useFetchParcelsReturn => {
   const [parcels, setParcels] = useState<Parcel[] | null>(null);
   const [earnings, setEarnings] = useState<number | null>(null);
 
-  const getNbItems = () => {
+  const getNbItems = useCallback(() => {
     const allItems = parcels?.map((parcel) => parcel.items).flat();
     const nbItems = allItems?.reduce((acc, item) => acc + item.quantity, 0);
     return nbItems;
-  };
+  }, [parcels]);
 
-  const getNbPaletes = () => {
+  const getNbPaletes = useCallback(() => {
     const paletteList = parcels?.map((parcel) => parcel.palette_number);
     const nbPalette = [...new Set(paletteList)].length;
     return nbPalette;
-  };
+  }, [parcels]);
 
-  const getParcelsFromPalette = (paletteNumber: number) => {
-    const paletteList = parcels?.filter(
-      (parcel) => parcel.palette_number === paletteNumber
-    );
-    return paletteList;
-  };
+  const getParcelsFromPalette = useCallback(
+    (paletteNumber: number) => {
+      const paletteList = parcels?.filter(
+        (parcel) => parcel.palette_number === paletteNumber
+      );
+      return paletteList;
+    },
+    [parcels]
+  );
 
   useEffect(() => {
     const getParcels = async () => {
